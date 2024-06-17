5e443c23bd8a99212323580451e826d2470dd833
bakhuiwon326 <powerup326@naver.com>
2024-06-17 22:06:05 +0900

    add member service project

---
+++ member/src/test/java/com/nhnacademy/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
